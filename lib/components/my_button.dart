03c4067a8d069e98cadd4543d398f17a99d9501a
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2023-06-08 19:47:53 +0500

    committed

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -3,21 +3,32 @@ import 'package:flutter/material.dart';
 class MyButton extends StatelessWidget {
   final String title;
   final Color color;
-  const MyButton({super.key, required this.title, this.color = const Color(0xff606C5D)});
+  final VoidCallback onPress;
+  const MyButton(
+      {super.key,
+      required this.title,
+      this.color = const Color(0xff606C5D),
+      required this.onPress});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        height: 80,
-        decoration:  BoxDecoration(
-          shape: BoxShape.circle,
-          color: color,
-        ),
-        child: Center(
-          child: Text(
-            title,
-            style: const TextStyle(fontSize: 20, color: Colors.white),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        child: InkWell(
+          onTap: onPress,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: color,
+            ),
+            child: Center(
+              child: Text(
+                title,
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
           ),
         ),
       ),
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,26 +9,111 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var userInput = '';
+  var answer = '';
   @override
   Widget build(BuildContext context) {
-    return const Directionality(
+    return Directionality(
       textDirection: TextDirection.ltr,
       child: Scaffold(
-        backgroundColor: Color(0xff1B1212),
+        backgroundColor: const Color(0xff1B1212),
         body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    MyButton(title: 'AC',),
-                    MyButton(title: '+/-',),
-                    MyButton(title: '%',),
-                    MyButton(title: '/', color: Color(0xff884A39),),
-                  ],
-                ),
-              ],
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Expanded(
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          MyButton(
+                            title: 'AC',
+                            onPress: () {},
+                          ),
+                          MyButton(
+                            title: '+/-',
+                            onPress: () {},
+                          ),
+                          MyButton(title: '%', onPress: () {}),
+                          MyButton(
+                              title: '/',
+                              color: const Color(0xff884A39),
+                              onPress: () {}),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          MyButton(
+                            title: '7',
+                            onPress: () {},
+                          ),
+                          MyButton(
+                            title: '8',
+                            onPress: () {},
+                          ),
+                          MyButton(title: '9', onPress: () {}),
+                          MyButton(
+                              title: 'x',
+                              color: const Color(0xff884A39),
+                              onPress: () {}),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          MyButton(
+                            title: '4',
+                            onPress: () {},
+                          ),
+                          MyButton(
+                            title: '5',
+                            onPress: () {},
+                          ),
+                          MyButton(title: '6', onPress: () {}),
+                          MyButton(
+                              title: '-',
+                              color: const Color(0xff884A39),
+                              onPress: () {}),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          MyButton(
+                            title: '1',
+                            onPress: () {},
+                          ),
+                          MyButton(
+                            title: '2',
+                            onPress: () {},
+                          ),
+                          MyButton(title: '3', onPress: () {}),
+                          MyButton(
+                              title: '+',
+                              color: const Color(0xff884A39),
+                              onPress: () {}),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          MyButton(
+                            title: '0',
+                            onPress: () {},
+                          ),
+                          MyButton(
+                            title: '.',
+                            onPress: () {},
+                          ),
+                          MyButton(title: 'DEL', onPress: () {}),
+                          MyButton(
+                              title: '=',
+                              color: const Color(0xff884A39),
+                              onPress: () {}),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
